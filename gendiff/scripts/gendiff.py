a6979d6355af50c1b22f4a7cd6e5c5b2158d2d3f
Maria Kulikova <maryku4815@gmail.com>
2026-02-11 16:21:35 +0300

    Step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,12 +6,18 @@ import argparse
 def main():
     """Точка входа утилиты gendiff."""
     parser = argparse.ArgumentParser(
-        usage='gendiff [-h] first_file second_file',
-        description='Compares two configuration files and shows a difference.',
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.print_help()
+    parser.add_argument('first_file', help='First file path')
+    parser.add_argument('second_file', help='Second file path')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help='set format of output'
+    )
+    parser.parse_args()
+
 
 
 if __name__ == "__main__":
